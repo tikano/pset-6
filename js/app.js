bb658e7f72dbebb5d3ca169623cf6532cf0b6d00
tikano <oadmathgenie@gmail.com>
2019-12-13 12:23:16 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,4 @@
 window.onload = function() {
-    document.getElementById("hello-sample").onclick = sayHelloStaff;
-    document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
-    document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
-    document.getElementById("triangle-sample").onclick = drawTriangleStaff;
-    document.getElementById("smile-sample").onclick = drawFaceStaff;
-    document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
-
-    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
-    //
-    // document.getElementById("some-id")   <-- you need to give each button a unique ID
-    //                                          and access it in this manner
-    //
-    // onclick is an event that is fired when you click something (in our case, a button).
-    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
-    // you should set onclick equal to your function names (i.e., sayHello).
-    //
-    // there are six event listeners being added for the staff solutions. you'll have an
-    // equivalent set of six event listeners for your solutions. the first one is done for you.
 
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
@@ -25,67 +7,34 @@ window.onload = function() {
     document.getElementById("smile").onclick = drawFace;
 }
 
-/*
- * Exercise 1.
- */
+class listObject{
+    var name = "";
+    var priority = "low";
+    var complete = false;
+    constructor(mname, mpriority){
+      name = mname;
+      priority = mpriority;
+    }
 
-const sayHello = function() {
-   const canvas = document.getElementById('student-canvas-1');
-   start();
-   ctx.font = "48px sans-serif";
-   
-   do {
-    var prompt = prompt("Message: ")
-    if (prompt.length > 50) {
-      alert("Your message is too long. Keep it under 50 characters")
+    getStatus(){
+        return complete;
     }
-  } while(prompt.length > 50 || prompt == null)
-      
-  ctx.strokeText(message, 30, 70, 994);
-  
-};
 
-/*
- * Exercise 2.
- */
+    getPriority(){
+        return priority;
+    }
 
-const drawRectangle = function() {
-    const canvas = document.getElementById('student-canvas-2');
-    start();
-    do {
-    var width = prompt("Width: ")
-    var height = prompt("Height: ")
-    var x = prompt("X: ")
-    var y = prompt("Y: ")
-    if (width == null || height == null || x == null || y == null) {
-      
+    getName(){
+        return name;
     }
-    if (width > 1024 || width < 1) {
-      alert("Your width must be between 1 and 1024.")
-    }
-    else if (height > 512 || height < 1) {
-      alert("Your height must be between 1 and 512.")
-    }
-    else if (x < 1 || x > 1024) {
-      alert("Your x-coordinate must be between 1 and 1024.")
-    }
-    else if (y < 1 || y > 512) {
-      alert("Your y-coordinate must be between 1 and 512.")
-    }
-    else if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
-      alert("One of your values is not a number.")
-    }
-    else if (Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512) {
-      alert("Your rectangle won't fit on the canvas.")
-    }
-  } while (width > 1024 || width < 1 || height > 512 || height < 1 || x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y) || Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512 || width == null || height == null || x == null || y == null)
-  ctx.beginPath();
-  ctx.rect(x, y, width, height);
-  ctx.closePath();
-  ctx.stroke();    
-};
 
-/*
- * Exercise 3.
- */
+    markComplete(){
+        complete = true;
+    }
 
+    markInComplete(){
+        complete = false;
+        
+    }
+
+}
