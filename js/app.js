cfa159675835b58e31e4d9ce9ddb0fe1e588f7b3
tikano <oadmathgenie@gmail.com>
2019-12-13 13:20:23 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,9 +5,8 @@ window.onload = function() {
     var name = document.getElementById("name").value;
     var priority = document.getElementById("priority").value;
     document.getElementById("input").onclick = construct(name, priority);
-    document.getElementById("input").onclick = writeItems;
     document.getElementById("remove").onclick = remove;
-    document.getElementById("remove").onclick = writeItems;
+    writeItems()
 }
 
 class listObject{
